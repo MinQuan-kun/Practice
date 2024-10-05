7efd9d3e2979e912bf79cd261e91705f375e657d
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-05 17:48:32 +0700

    Xuất cơ bản

---
+++ Upcoder/Lập trình cơ bản/1.Nhập + Xuất/Xuất cơ bản 2/XUAT2.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "1" << endl;
+    cout << "12" << endl;
+    cout << "123" << endl;
+    cout << "1234" << endl;
+    cout << "12345" << endl;
+    return 0;
+}
---
+++ Upcoder/Lập trình cơ bản/1.Nhập + Xuất/Xuất cơ bản 2/XUAT2.cs
@@ -0,0 +1,16 @@
+using System;
+namespace xuatcb
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("12");
+            Console.WriteLine("123");
+            Console.WriteLine("1234");
+            Console.Write("12345");
+        }
+    }
+    
+}
