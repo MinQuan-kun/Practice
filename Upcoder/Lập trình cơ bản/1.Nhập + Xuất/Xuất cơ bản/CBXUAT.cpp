dcc040be7d063e654b38380b847947b51f87ff9f
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-05 17:40:54 +0700

    Xuất cơ bản

---
+++ Upcoder/Lập trình cơ bản/1.Nhập + Xuất/Xuất cơ bản/CBXUAT.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++/h>
+using namespace std;
+int main()
+{
+    cout << "EM XIN HUA: KHONG BAO GIO" << endl;
+    cout << "COPY BAI (hoac chia se bai)" << endl;
+    cout << "NEU VI PHAM SE BI 0 DIEM TOAN BAI THI";
+}
---
+++ Upcoder/Lập trình cơ bản/1.Nhập + Xuất/Xuất cơ bản/CBXUAT.cs
@@ -0,0 +1,13 @@
+using System;
+namespace xuatcp
+{
+    public class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("EM XIN HUA: KHONG BAO GIO");
+            Console.WriteLine("COPY BAI (hoac chia se bai)");
+            Console.Write("NEU VI PHAM SE BI 0 DIEM TOAN BAI THI");
+        }
+    }
+}
