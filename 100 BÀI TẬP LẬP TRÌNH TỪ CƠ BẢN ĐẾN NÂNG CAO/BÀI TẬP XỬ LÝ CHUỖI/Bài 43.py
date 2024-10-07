e76448df7d33ec7d413a3ffa10ce115acbea8eca
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-07 16:36:34 +0700

    sua lai ghi chu

--- 100 BÀI TẬP LẬP TRÌNH TỪ CƠ BẢN ĐẾN NÂNG CAO/BÀI TẬP XỬ LÝ CHUỖI/Bài 43.py
+++ 100 BÀI TẬP LẬP TRÌNH TỪ CƠ BẢN ĐẾN NÂNG CAO/BÀI TẬP XỬ LÝ CHUỖI/Bài 43.py
@@ -1,1 +1,1 @@
-#rong chuỗi có bao nhiêu từ (quy định là chuỗi không có ký tự đặc biệt, không số, không có dấu câu, chỉ có ký tự chữ và khoảng trắng)
+#Trong chuỗi có bao nhiêu từ (quy định là chuỗi không có ký tự đặc biệt, không số, không có dấu câu, chỉ có ký tự chữ và khoảng trắng)
