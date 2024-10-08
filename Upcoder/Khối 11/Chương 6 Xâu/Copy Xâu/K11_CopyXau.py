e831b5d4e7e52ab539f2c017a6f658009c77b873
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-08 15:45:28 +0700

    đã cập nhật code full điểm

--- Upcoder/Khối 11/Chương 6 Xâu/Copy Xâu/K11_CopyXau.py
+++ Upcoder/Khối 11/Chương 6 Xâu/Copy Xâu/K11_CopyXau.py
@@ -1,7 +1,7 @@
 #code này chỉ có Điểm 3.75
 a = input()
 n, m = map(int, input().split())
-if n < 0 or m > len(a):
+if n < 0 or m > len(a) or m < 0 or m >= len(a):
     print("gioi han khong dung", end="")
 else:
     sub_string = a[slice(n, m + 1)]
