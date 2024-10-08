90d010957ac923e11492fa7fa0e8a62104577e34
Ikkun2705 <Mr.fariol2705@gmail.com>
2024-10-08 15:14:42 +0700

    Đếm từ

---
+++ Upcoder/Khối 11/Chương 6 Xâu/Đếm từ/K11_DemTu.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a;
+    vector<string> v1;
+    while(cin >> a)
+    v1.push_back(a);
+    cout << v1.size();
+    return 0;
+}
